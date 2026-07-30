1d6438c2cb73cc9c686e21a5b3c6bb1f4d43ce3d
7savv7 <schrysostomid@gmail.com>
2026-07-30 20:48:56 +0300

    Add QR Code Component challenge

---
+++ newbie/qr-code-component/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      Improve your front-end skills by building projects Scan the QR code to
+      visit Frontend Mentor and take your coding skills to the next level
+    </div>
+  );
+}
+
+export default App;
