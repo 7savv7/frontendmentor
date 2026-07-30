c7de36f9bbe9d4e325893b8de694ab8b7d8149c9
7savv7 <schrysostomid@gmail.com>
2026-07-30 21:35:13 +0300

    Finalize QR Code Component challenge

--- newbie/qr-code-component/src/App.tsx
+++ newbie/qr-code-component/src/App.tsx
@@ -2,10 +2,16 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      Improve your front-end skills by building projects Scan the QR code to
-      visit Frontend Mentor and take your coding skills to the next level
-    </div>
+    <main className="card">
+      <img src="image-qr-code.png" alt="qr-code" />
+
+      <h1>Improve your front-end skills by building projects</h1>
+
+      <p>
+        Scan the QR code to visit Frontend Mentor and take your coding skills to
+        the next level
+      </p>
+    </main>
   );
 }
 
