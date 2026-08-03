a1ff491c036b70b26c58f76811f1cbdf0fca16b1
7savv7 <schrysostomid@gmail.com>
2026-08-03 23:23:47 +0300

    [newbie/blog-preview-card]: Finalize project

--- newbie/blog-preview-card/src/App.tsx
+++ newbie/blog-preview-card/src/App.tsx
@@ -1,15 +1,34 @@
-import "./App.css";
-
 function App() {
-  return <div>  Learning
+  return (
+    <main className="bg-white rounded-2xl p-5 w-full max-w-80 border shadow-[10px_10px_0px_0px_black]">
+      <img
+        className="rounded-xl"
+        src="/images/illustration-article.svg"
+        alt="illustration"
+      />
 
-  Published 21 Dec 2023
+      <div className="bg-yellow mt-5 rounded-md p-1 px-4 w-fit font-bold">
+        <p>Learning</p>
+      </div>
 
-  HTML & CSS foundations
+      <p className="mt-3 text-sm font-medium">Published 21 Dec 2023</p>
 
-  These languages are the backbone of every website, defining structure, content, and presentation.
+      <h1 className="mt-4 font-extrabold text-xl cursor-pointer hover:text-yellow">
+        HTML & CSS foundations
+      </h1>
 
-  Greg Hooper</div>;
+      <p className="mt-3 text-g500 text-sm font-medium">
+        These languages are the backbone of every website, defining structure,
+        content, and presentation.
+      </p>
+
+      <div className="flex gap-2 items-center mt-4">
+        <img className="w-7 h-7" src="images/image-avatar.webp" alt="avatar" />
+
+        <p className="font-bold">Greg Hooper</p>
+      </div>
+    </main>
+  );
 }
 
 export default App;
