0a8dd4e689f51ac4ee38cb1c3d18bc748cec24ce
7savv7 <schrysostomid@gmail.com>
2026-08-05 20:15:04 +0300

    [nft-preview-card-component]: Initial commit

---
+++ newbie/nft-preview-card-component/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      Equilibrium #3429 Our Equilibrium collection promotes balance and calm.
+      0.041 ETH 3 days left Creation of Jules Wyvern
+    </div>
+  );
+}
+
+export default App;
