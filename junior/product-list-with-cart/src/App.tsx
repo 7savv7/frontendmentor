0cec2f89df1f23946e25e918a6e9b57b99184528
7savv7 <schrysostomid@gmail.com>
2026-08-05 03:21:21 +0300

    [product-list-with-cart]: Add cart functionality, card layout and styles

--- junior/product-list-with-cart/src/App.tsx
+++ junior/product-list-with-cart/src/App.tsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
 import data from "./data.json";
 import Dessert from "./Dessert";
 
+export interface Des {
+  image: {
+    thumbnail: string;
+    mobile: string;
+    tablet: string;
+    desktop: string;
+  };
+  name: string;
+  category: string;
+  price: number;
+}
+
 function App() {
+  const [cart, setCart] = useState<(Des & { quantity: number })[]>([]);
+
   return (
     <div>
       <main>
-        <h1 className="text-3xl font-bold">Desserts</h1>
-        <section>
+        <h1 className="text-3xl font-bold mb-5">Desserts</h1>
+
+        <section className="grid gap-5 grid-cols-[repeat(auto-fill,minmax(280px,1fr))] lg:grid-cols-3">
           {data.map((dessert) => (
-            <Dessert dessert={dessert} />
+            <Dessert
+              key={dessert.name}
+              dessert={dessert}
+              cart={cart}
+              setCart={setCart}
+            />
           ))}
         </section>
       </main>
--- junior/product-list-with-cart/src/Dessert.tsx
+++ junior/product-list-with-cart/src/Dessert.tsx
@@ -1,46 +1,116 @@
-import { useState } from "react";
+import { type Dispatch, type SetStateAction } from "react";
+import type { Des } from "./App";
 
-interface Des {
-  image: {
-    thumbnail: string;
-    mobile: string;
-    tablet: string;
-    desktop: string;
-  };
-  name: string;
-  category: string;
-  price: number;
+interface Props {
+  dessert: Des;
+  cart: (Des & { quantity: number })[];
+  setCart: Dispatch<SetStateAction<(Des & { quantity: number })[]>>;
 }
 
-function Dessert({ dessert }: { dessert: Des }) {
-  const [inCart, setInCart] = useState<boolean>(true);
+function Dessert({ dessert, cart, setCart }: Props) {
+  const inCart = cart?.find((des) => des.name === dessert.name);
+
+  const addToCart = () => {
+    setCart((prev) => {
+      return [...prev, { ...dessert, quantity: 1 }];
+    });
+  };
+
+  const decreaseQuantity = () => {
+    setCart((prev) =>
+      inCart?.quantity === 1
+        ? prev.filter((des) => des.name !== dessert.name)
+        : prev.map((des) =>
+            des.name === dessert.name && des.quantity > 1
+              ? { ...des, quantity: des.quantity - 1 }
+              : des,
+          ),
+    );
+  };
+
+  const increaseQuantity = () => {
+    setCart((prev) =>
+      prev.map((des) =>
+        des.name === dessert.name
+          ? { ...des, quantity: des.quantity + 1 }
+          : des,
+      ),
+    );
+  };
 
   return (
-    <div>
-      <div className="relative flex flex-col items-center">
+    <div className="flex flex-col w-fit mb-5">
+      <div className="relative flex flex-col items-center mb-6">
         <img
-          className="rounded-2xl"
+          className={`rounded-2xl outline-3 ${inCart ? "outline-r" : "outline-transparent"}`}
           src={dessert.image.mobile}
           alt={`${dessert.name}-image`}
         />
 
-        {inCart ? (
-          <button className="flex cursor-pointer gap-2 content-center items-center absolute top-[calc(100%-19px)] h-10 rounded-4xl border px-4 py-2 bg-white">
-            <img
-              src="images/icon-add-to-cart.svg"
-              alt={`${dessert.name}-add-to-cart`}
-            />
-            <p>Add to Cart</p>
-          </button>
-        ) : (
-          <div></div>
-        )}
+        <div className="absolute top-[calc(100%-19px)] h-10 w-40">
+          {inCart ? (
+            <div className="flex gap-8 justify-between items-center h-full w-full rounded-4xl border border-transparent px-4 py-2 bg-r">
+              <button
+                onClick={decreaseQuantity}
+                className="group cursor-pointer border border-white rounded-full p-1 py-2 hover:bg-white"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  height="2"
+                  fill="none"
+                  viewBox="0 0 10 2"
+                >
+                  <path
+                    className="group-hover:fill-r"
+                    fill="#fff"
+                    d="M0 .375h10v1.25H0V.375Z"
+                  />
+                </svg>
+              </button>
+
+              <p className="text-white">{inCart?.quantity}</p>
+
+              <button
+                onClick={increaseQuantity}
+                className="group cursor-pointer border border-white rounded-full p-1 hover:bg-white"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  height="10"
+                  fill="none"
+                  viewBox="0 0 10 10"
+                >
+                  <path
+                    className="group-hover:fill-r"
+                    fill="#fff"
+                    d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"
+                  />
+                </svg>
+              </button>
+            </div>
+          ) : (
+            <button
+              onClick={addToCart}
+              className="group flex cursor-pointer gap-2 items-center h-full w-full rounded-4xl border border-rose500 px-4 py-2 bg-white hover:border-r"
+            >
+              <img
+                src="images/icon-add-to-cart.svg"
+                alt={`${dessert.name}-add-to-cart`}
+              />
+              <p className="font-semibold text-rose900 group-hover:text-r">
+                Add to Cart
+              </p>
+            </button>
+          )}
+        </div>
       </div>
 
       <div>
-        <p>{dessert.category}</p>
-        <p>{dessert.name}</p>
-        <p>${dessert.price.toFixed(2)}</p>
+        <p className="text-rose400">{dessert.category}</p>
+        <p className="font-semibold text-rose900">{dessert.name}</p>
+        <p className="font-semibold text-r">${dessert.price.toFixed(2)}</p>
       </div>
     </div>
   );
