366772121c87bdb88c99bbc22a6c991e276ac23a
7savv7 <schrysostomid@gmail.com>
2026-08-08 21:36:56 +0300

    [time-tracking-dashboard]: Finalize project

--- junior/time-tracking-dashboard/src/App.tsx
+++ junior/time-tracking-dashboard/src/App.tsx
@@ -55,21 +55,21 @@ function App() {
   const [period, setPeriod] = useState<Period>("weekly");
 
   return (
-    <div className="bg-navy950 h-full w-full flex justify-center items-center  p-10">
-      <div className="grid grid-cols-4 content-center items-stretch gap-5 max-w-250 w-full h-full">
+    <div className="bg-navy950 min-h-screen h-fit w-full flex justify-center items-center p-10">
+      <div className="grid grid-cols-4 content-center items-stretch gap-5 max-w-250 w-full h-full max-md:grid-cols-1">
         <div className="bg-navy900 rounded-xl row-span-2">
-          <div className="bg-purple600 h-[70%] rounded-xl p-6">
-            <img className="h-10 w-10" src="/images/image-jeremy.png" alt="image-jeremy" />
-            <p>
-              Report for <span>Jeremy Robson</span>
+          <div className="bg-purple600 h-[70%] rounded-xl p-6 flex flex-col gap-8 max-md:flex-row max-md:gap-5 max-md:h-fit">
+            <img className="h-16 w-16 border-3 rounded-full border-white" src="/images/image-jeremy.png" alt="image-jeremy" />
+            <p className="text-navy200 flex flex-col">
+              Report for <span className="text-white text-[2em] leading-[1.2em] max-md:text-[1.6em]">Jeremy Robson</span>
             </p>
           </div>
 
-          <div className="flex flex-col h-[30%] gap-2 p-6">
+          <div className="flex flex-col h-[30%] gap-2 p-6 max-md:flex-row">
             {["daily", "weekly", "monthly"].map((p) => (
               <p
                 key={p}
-                className={`cursor-pointer ${period === p ? "text-white" : "text-navy200"} capitalize hover:text-white w-fit`}
+                className={`cursor-pointer ${period === p ? "text-white" : "text-navy200"} capitalize hover:text-white w-fit max-md:flex-1 text-center`}
                 onClick={() => setPeriod(p as Period)}
               >
                 {p}
--- junior/time-tracking-dashboard/src/Card.tsx
+++ junior/time-tracking-dashboard/src/Card.tsx
@@ -15,24 +15,36 @@ function Card({ data, cardData, period }: Props) {
   return (
     <div
       className={`${cardData.color} h-50 flex flex-col justify-end rounded-xl rounded-b-3xl 
-      ${cardData.image} bg-position-[90%_-10%] bg-no-repeat`}
+      ${cardData.image} bg-position-[90%_-10%] bg-no-repeat max-md:h-40`}
     >
-      <div className="h-[80%] bg-navy900 p-6 flex flex-col justify-between rounded-xl text-white">
+      <div className="cursor-pointer h-[80%] bg-navy900 p-6 flex flex-col gap-2 justify-between rounded-xl text-white text-[18px] hover:bg-[hsl(235,46%,30%)]">
         <div className="flex items-center justify-between">
           <p>{data.title}</p>
-          <img className="cursor-pointer" src="images/icon-ellipsis.svg" alt="icon-ellipsis" />
+          <svg
+            className="group cursor-pointer"
+            width="21"
+            height="5"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              className="group-hover:fill-white"
+              d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
+              fill="#BBC0FF"
+              fillRule="evenodd"
+            />
+          </svg>
         </div>
 
-        <div>
-          <p>{data.timeframes[period].current}hrs</p>
+        <div className="flex flex-col max-md:flex-row max-md:justify-between max-md:items-center">
+          <p className="text-[2em]">{data.timeframes[period].current}hrs</p>
 
-          <p>
+          <p className="text-navy200 text-[0.8em]">
             {period === "daily"
               ? "Yesterday"
               : period === "weekly"
                 ? "Last Week"
-                : "Last Month"}
-            -{data.timeframes[period].previous}hrs
+                : "Last Month"}{" "}
+            - {data.timeframes[period].previous}hrs
           </p>
         </div>
       </div>
