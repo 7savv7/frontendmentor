5c30df2396e3fa2efd07f528e39f93f94eb62b8f
7savv7 <schrysostomid@gmail.com>
2026-08-09 19:37:10 +0300

    [todo-app]: Initial commit

---
+++ intermediate/todo-app/src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
+
+/* 
+  Todo
+
+  <!-- Add dynamic number --> items left
+
+  All
+  Active 
+  Completed
+
+  Clear Completed
+
+  Drag and drop to reorder list */
