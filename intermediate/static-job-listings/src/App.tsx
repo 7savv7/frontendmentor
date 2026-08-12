ea8a18b2072817fe15d5bb7815841c5616d0ba41
7savv7 <schrysostomid@gmail.com>
2026-08-12 18:06:25 +0300

    [static-job-listings]: Add background

--- intermediate/static-job-listings/src/App.tsx
+++ intermediate/static-job-listings/src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-  return <div>hey</div>;
+  return (
+    <div className="bg-background min-h-svh md:min-h-screen">
+      <div
+        className="relative h-[156px] w-full bg-green400 bg-[url(/images/bg-header-mobile.svg)]
+     md:bg-[url(/images/bg-header-desktop.svg)]"
+      ></div>
+    </div>
+  );
 }
 
 export default App;
