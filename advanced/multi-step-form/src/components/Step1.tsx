66e129b6465ea77d441d7b6d2c6d33304ce9324d
7savv7 <schrysostomid@gmail.com>
2026-08-29 19:22:26 +0300

    [multi-step-form]: Improve styles of first 2 steps

--- advanced/multi-step-form/src/components/Step1.tsx
+++ advanced/multi-step-form/src/components/Step1.tsx
@@ -1,22 +1,41 @@
 function Step1() {
   return (
-    <div>
-      <h1>Personal info</h1>
-      <p>Please provide your name, email address, and phone number.</p>
+    <div className="text-blue950">
+      <h1 className="text-[1.5em] font-bold">Personal info</h1>
+      <p className="mt-2 text-grey500">
+        Please provide your name, email address, and phone number.
+      </p>
 
-      <div>
-        <label htmlFor="name">Name</label>
-        <input id="name" type="text" placeholder="e.g. Stephen King" />
+      <div className="flex flex-col gap-3 mt-5">
+        <div className="flex flex-col item-start">
+          <label htmlFor="name">Name</label>
+          <input
+            className="border border-grey500 text-grey500 font-[500] p-2 px-4 rounded-sm outline-none"
+            id="name"
+            type="text"
+            placeholder="e.g. Stephen King"
+          />
+        </div>
 
-        <label htmlFor="email">Email Address</label>
-        <input
-          id="email"
-          type="text"
-          placeholder="e.g. stephenking@lorem.com"
-        />
+        <div className="flex flex-col item-start">
+          <label htmlFor="email">Email Address</label>
+          <input
+            className="border border-grey500 text-grey500 font-[500] p-2 px-4 rounded-sm outline-none"
+            id="email"
+            type="text"
+            placeholder="e.g. stephenking@lorem.com"
+          />
+        </div>
 
-        <label htmlFor="phone">Phone Number</label>
-        <input id="phone" type="text" placeholder="e.g. +1 234 567 890" />
+        <div className="flex flex-col item-start">
+          <label htmlFor="phone">Phone Number</label>
+          <input
+            className="border border-grey500 text-grey500 font-[500] p-2 px-4 rounded-sm outline-none"
+            id="phone"
+            type="text"
+            placeholder="e.g. +1 234 567 890"
+          />
+        </div>
       </div>
     </div>
   );
--- advanced/multi-step-form/src/components/Step2.tsx
+++ advanced/multi-step-form/src/components/Step2.tsx
@@ -1,30 +1,71 @@
+import { useState } from "react";
+
 function Step2() {
+  const [yearly, setYearly] = useState<boolean>(false);
+
   return (
-    <div>
-      <h1>Select your plan</h1>
-      <p>You have the option of monthly or yearly billing.</p>
+    <div className="text-blue950">
+      <h1 className="text-[1.5em] font-bold">Select your plan</h1>
+      <p className="mt-2 text-grey500">
+        You have the option of monthly or yearly billing.
+      </p>
 
-      <div>
-        <div>
-          <p>Arcade</p>
-          <p>$9/mo</p>
+      <div className="flex flex-col gap-3 mt-5">
+        <div className="flex items-start gap-3 border border-grey500 rounded-md p-3">
+          <img src="/images/icon-arcade.svg" alt="arcade" />
+
+          <div>
+            <p>Arcade</p>
+            <p>$9/mo</p>
+            <p className="hidden">2 months free</p>
+          </div>
         </div>
 
-        <div>
-          <p>Advanced</p>
-          <p>$12/mo</p>
+        <div className="flex items-start gap-3 border border-grey500 rounded-md p-3">
+          <img src="/images/icon-advanced.svg" alt="advanced" />
+          <div>
+            <p>Advanced</p>
+            <p>$12/mo</p>
+            <p className="hidden">2 months free</p>
+          </div>
         </div>
 
-        <div>
-          <p>Pro</p>
-          <p>$15/mo</p>
+        <div className="flex items-start gap-3 border border-grey500 rounded-md p-3">
+          <img src="/images/icon-pro.svg" alt="pro" />
+          <div>
+            <p>Pro</p>
+            <p>$15/mo</p>
+            <p className="hidden">2 months free</p>
+          </div>
         </div>
       </div>
 
-      <div>
-        <p>Monthly</p>
+      <div className="flex items-center justify-center text-grey500 font-[500] gap-5 p-2 px-5 mt-5 bg-blue50 rounded-lg">
+        <button
+          type="button"
+          onClick={() => setYearly(false)}
+          className={`${!yearly && "text-blue950"} transition-all`}
+        >
+          Monthly
+        </button>
 
-        <p>Yearly</p>
+        <div
+          onClick={() => setYearly((prev) => !prev)}
+          className="relative bg-blue950 h-5 w-10 rounded-full"
+        >
+          <div
+            className={`absolute w-[12px] top-[4px] h-[12px] bg-white rounded-full 
+              ${yearly ? "left-[calc(100%-4px)] -translate-x-full" : "left-[4px]"} transition-all`}
+          />
+        </div>
+
+        <button
+          type="button"
+          onClick={() => setYearly(true)}
+          className={`${yearly && "text-blue950"} transition-all`}
+        >
+          Yearly
+        </button>
       </div>
     </div>
   );
