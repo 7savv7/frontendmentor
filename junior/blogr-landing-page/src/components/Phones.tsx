a657baaada8e20de3d53d01d954e19bca566d498
7savv7 <schrysostomid@gmail.com>
2026-09-19 18:27:09 +0300

    [blogr-landing-page]: Make phones component responsive and improve styles

--- junior/blogr-landing-page/src/components/Phones.tsx
+++ junior/blogr-landing-page/src/components/Phones.tsx
@@ -1,23 +1,29 @@
 function Phones() {
   return (
-    <div className="text-white -mt-50 text-center">
+    <div
+      className="flex flex-col items-center text-white -mt-50 text-center lg:flex-row lg:mt-0
+      lg:relative"
+    >
       <img
-        className="relative top-50 z-1"
+        className="relative top-50 z-1 lg:-top-[16%] lg:left-10 lg:absolute"
         src="/images/illustration-phones.svg"
         alt="phones"
       />
 
-      <div className="overflow-hidden relative bg-linear-to-b from-purple950 to-purple900 pt-50 pb-20 p-5  rounded-tr-[80px] rounded-bl-[80px]">
+      <div
+        className="overflow-hidden relative bg-linear-to-b from-purple950 to-purple900 pt-50 pb-20 p-10 
+      rounded-tr-[80px] rounded-bl-[80px] lg:pl-[50%] lg:text-left lg:pt-34 lg:pb-34"
+      >
         <img
-          className="absolute bottom-[40%]"
+          className="absolute bottom-[40%] lg:-left-[18%] lg:bottom-0"
           src="/images/bg-pattern-circles.svg"
           alt="circles"
         />
 
-        <h2 className="relative z-1 text-[1.6em]">
+        <h2 className="relative z-1 text-[2em]">
           State of the Art Infrastructure
         </h2>
-        <p className="relative z-1 mt-5">
+        <p className="relative z-1 mt-5 text-white/70">
           With reliability and speed in mind, worldwide data centers provide the
           backbone for ultra-fast connectivity. This ensures your site will load
           instantly, no matter where your readers are, keeping your site
