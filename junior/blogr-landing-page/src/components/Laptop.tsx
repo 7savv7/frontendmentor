52018e8ea77bff2a876ad24c713f56d54137385c
7savv7 <schrysostomid@gmail.com>
2026-09-23 22:50:02 +0300

    [blogr-landing-page]: Make Laptop component responsive

--- junior/blogr-landing-page/src/components/Laptop.tsx
+++ junior/blogr-landing-page/src/components/Laptop.tsx
@@ -1,7 +1,7 @@
 function Laptop() {
   return (
-    <div className="flex flex-col items-center gap-5 text-center p-5">
-      <picture className="lg:w-1/2 lg:relative lg:-right-70">
+    <div className="flex flex-col items-center gap-10 text-center p-10 lg:flex-row lg:py-30">
+      <picture className="lg:w-1/2 lg:relative lg:right-50">
         <source
           media="(min-width: 1024px)"
           srcSet="/images/illustration-laptop-desktop.svg"
@@ -9,10 +9,10 @@ function Laptop() {
         <img src="/images/illustration-laptop-mobile.svg" alt="laptop" />
       </picture>
 
-      <div className="flex flex-col items-center gap-5">
+      <div className="flex flex-col items-center gap-10 lg:text-left lg:w-1/2 lg:pr-50">
         <div>
-          <h2 className="text-[1.4em] font-[600] text-blue900">Free, open, simple</h2>
-          <p>
+          <h2 className="text-[1.4em] font-[600] text-blue900 lg:text-[1.8em]">Free, open, simple</h2>
+          <p className="text-gray600 mt-5">
             Blogr is a free and open source application backed by a large
             community of helpful developers. It supports features such as code
             syntax highlighting, RSS feeds, social media integration,
@@ -23,8 +23,8 @@ function Laptop() {
         </div>
 
         <div>
-          <h2 className="text-[1.4em] font-[600] text-blue900">Powerful tooling</h2>
-          <p>
+          <h2 className="text-[1.4em] font-[600] text-blue900 lg:text-[1.8em]">Powerful tooling</h2>
+          <p className="text-gray600 mt-5">
             Batteries included. We built a simple and straightforward CLI tool
             that makes customization and deployment a breeze, but capable of
             producing even the most complicated sites.
