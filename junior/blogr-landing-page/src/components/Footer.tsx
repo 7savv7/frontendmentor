b1cc85d224275483bc066b0dace7a632804af6c9
7savv7 <schrysostomid@gmail.com>
2026-09-25 17:18:43 +0300

    [blogr-landing-page]: Make footer responsive

--- junior/blogr-landing-page/src/components/Footer.tsx
+++ junior/blogr-landing-page/src/components/Footer.tsx
@@ -1,12 +1,15 @@
 function Footer() {
   return (
-    <footer className="bg-gray900 rounded-tr-[80px] flex flex-col items-center gap-10 p-10 text-white text-center">
+    <footer
+      className="bg-gray900 rounded-tr-[80px] flex flex-col items-center gap-10 p-10 text-white text-center
+      lg:flex-row lg:justify-evenly lg:items-start lg:text-left [&_li]:cursor-pointer [&_li]:hover:underline"
+    >
       <img src="/images/logo.svg" alt="logo" />
 
       <div>
         <h3>Product</h3>
 
-        <ul>
+        <ul className="mt-5 flex flex-col gap-2 text-white/60">
           <li>Overview</li>
           <li>Pricing</li>
           <li>Marketplace</li>
@@ -18,7 +21,7 @@ function Footer() {
       <div>
         <h3>Company</h3>
 
-        <ul>
+        <ul className="mt-5 flex flex-col gap-2 text-white/60">
           <li>About</li>
           <li>Team</li>
           <li>Blog</li>
@@ -29,7 +32,7 @@ function Footer() {
       <div>
         <h3>Connect</h3>
 
-        <ul>
+        <ul className="mt-5 flex flex-col gap-2 text-white/60">
           <li>Contact</li>
           <li>Newsletter</li>
           <li>LinkedIn</li>
